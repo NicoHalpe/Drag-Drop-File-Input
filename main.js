2420c8fead307f5ab2ed8ca488aa331f0e266653
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2021-09-22 19:06:47 -0300

    .

--- main.js
+++ main.js
@@ -5,52 +5,106 @@ var dropbox = document.getElementById("dropzone");
 
 inputElement.addEventListener("change", () => handleFiles(inputElement.files), false);
 
-function handleFiles(fileList) {
+const plusPlaceholder = document.createElement("div");
+plusPlaceholder.classList.add("plus-placeholder");
+plusPlaceholder.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+<path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
+</svg>
+<span>Agrega otra imagen</span>
+`;
+
+const archivosRepetidos = {}
+
+async function handleFiles(fileList) {
 	for (let i = 0; i < fileList.length; i++) {
-		if (files.length === 0) dropbox.classList.add("files");
+		if (files.length === 0) {
+			dropbox.classList.add("files");
+			dropbox.appendChild(plusPlaceholder);
+		}
 
-		const filePrev = document.createElement("div");
-        filePrev.onclick = (e) => {e.stopPropagation()}
-		filePrev.classList.add("item");
-        filePrev.setAttribute("index", files.length)
-		filePrev.innerHTML = `
-        <img src="https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_image_x16.png" />
-        <span>${fileList[i].name}</span>
-        <svg class="close" xmlns="http://www.w3.org/2000/svg" width="16" viewBox="4 4 8 8" onclick="handleItemRemove(this)">
-            <path d="M 4.646 4.646 a 0.5 0.5 0 0 1 0.708 0 L 8 7.293 l 2.646 -2.647 a 0.5 0.5 0 0 1 0.708 0.708 L 8.707 8 l 2.647 2.646 a 0.5 0.5 0 0 1 -0.708 0.708 L 8 8.707 l -2.646 2.647 a 0.5 0.5 0 0 1 -0.708 -0.708 L 7.293 8 L 4.646 5.354 a 0.5 0.5 0 0 1 0 -0.708 z" fill="#000000"/>
-        </svg>
-        `;
+		const repeated = files.find((obj) => obj.name === fileList[i].name);
 
-		dropbox.appendChild(filePrev);
+		if (repeated) {
+			if (confirm(`${fileList[i].name} ya existe, deseas agregarlo de todas formas?`)) {
+                if(!archivosRepetidos[fileList[i].name]) archivosRepetidos[fileList[i].name] = 1
+                else archivosRepetidos[fileList[i].name] = archivosRepetidos[fileList[i].name] + 1
+				var blob = fileList[i].slice(0, fileList[i].size, "image/png");
+				const newFile = new File([blob], fileList[i].name.split(".")[0] + ` (${archivosRepetidos[fileList[i].name]}).` + fileList[i].name.split(".")[1], { type: "image/png" });
+				const filePrev = document.createElement("div");
+				filePrev.onclick = (e) => {
+					e.stopPropagation();
+				};
+				filePrev.classList.add("item");
+				filePrev.setAttribute("index", files.length);
+				const base64 = await getBase64(fileList[i]);
+				filePrev.innerHTML = `
+                    <img src="${base64}" height="16" />
+                    <span>${newFile.name}</span>
+                    <svg class="close" xmlns="http://www.w3.org/2000/svg" width="16" viewBox="4 4 8 8" onclick="handleItemRemove(this)">
+                        <path d="M 4.646 4.646 a 0.5 0.5 0 0 1 0.708 0 L 8 7.293 l 2.646 -2.647 a 0.5 0.5 0 0 1 0.708 0.708 L 8.707 8 l 2.647 2.646 a 0.5 0.5 0 0 1 -0.708 0.708 L 8 8.707 l -2.646 2.647 a 0.5 0.5 0 0 1 -0.708 -0.708 L 7.293 8 L 4.646 5.354 a 0.5 0.5 0 0 1 0 -0.708 z" fill="#000000"/>
+                    </svg>
+                `;
 
-        files.push(fileList[i]);
+				dropbox.insertBefore(filePrev, plusPlaceholder);
+				dropbox.scrollTo({ top: dropbox.scrollHeight, behavior: "smooth" });
+				files.push(newFile);
+			}
+		} else {
+			const filePrev = document.createElement("div");
+			filePrev.onclick = (e) => {
+				e.stopPropagation();
+			};
+			filePrev.classList.add("item");
+			filePrev.setAttribute("index", files.length);
+			const base64 = await getBase64(fileList[i]);
+			filePrev.innerHTML = `
+                <img src="${base64}" height="16" />
+                <span>${fileList[i].name}</span>
+                <svg class="close" xmlns="http://www.w3.org/2000/svg" width="16" viewBox="4 4 8 8" onclick="handleItemRemove(this)">
+                    <path d="M 4.646 4.646 a 0.5 0.5 0 0 1 0.708 0 L 8 7.293 l 2.646 -2.647 a 0.5 0.5 0 0 1 0.708 0.708 L 8.707 8 l 2.647 2.646 a 0.5 0.5 0 0 1 -0.708 0.708 L 8 8.707 l -2.646 2.647 a 0.5 0.5 0 0 1 -0.708 -0.708 L 7.293 8 L 4.646 5.354 a 0.5 0.5 0 0 1 0 -0.708 z" fill="#000000"/>
+                </svg>
+            `;
+
+			dropbox.insertBefore(filePrev, plusPlaceholder);
+			dropbox.scrollTo({ top: dropbox.scrollHeight, behavior: "smooth" });
+			files.push(fileList[i]);
+		}
 	}
 }
 
-/**
- * 
- * @param {HTMLElement} item 
- */
-function handleItemRemove(item) {
-    const index = item.parentElement.getAttribute("index")
-    files.splice(index, 1)
-    item.parentElement.parentElement.innerHTML = item.parentElement.parentElement.children[0].outerHTML + item.parentElement.parentElement.children[1].outerHTML
-    files.map((item, index) => {
-        const filePrev = document.createElement("div");
-        filePrev.onclick = (e) => {e.stopPropagation()}
-		filePrev.classList.add("item");
-        filePrev.setAttribute("index", index)
-		filePrev.innerHTML = `
-        <img src="https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_image_x16.png" />
-        <span>${item.name}</span>
-        <svg class="close" xmlns="http://www.w3.org/2000/svg" width="16" viewBox="4 4 8 8" onclick="handleItemRemove(this)">
-            <path d="M 4.646 4.646 a 0.5 0.5 0 0 1 0.708 0 L 8 7.293 l 2.646 -2.647 a 0.5 0.5 0 0 1 0.708 0.708 L 8.707 8 l 2.647 2.646 a 0.5 0.5 0 0 1 -0.708 0.708 L 8 8.707 l -2.646 2.647 a 0.5 0.5 0 0 1 -0.708 -0.708 L 7.293 8 L 4.646 5.354 a 0.5 0.5 0 0 1 0 -0.708 z" fill="#000000"/>
-        </svg>
-        `;
+const getBase64 = (file) => {
+	return new Promise((resolve) => {
+		let baseURL = "";
+		let reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			baseURL = reader.result;
+			resolve(baseURL);
+		};
+	});
+};
 
-		dropbox.appendChild(filePrev);
-    })
-    if (files.length === 0) dropbox.classList.remove("files");
+async function handleItemRemove(item) {
+	const index = parseInt(item.parentElement.getAttribute("index"));
+	files.splice(index, 1);
+	console.log(
+		$(dropbox)
+			.children()
+			.eq(index + 2)
+	);
+	$(dropbox)
+		.children()
+		.eq(index + 2)
+		.remove();
+
+	files.map(async (item, index) => {
+		dropbox.children[index + 2].setAttribute("index", index);
+	});
+	if (files.length === 0) {
+		dropbox.classList.remove("files");
+		dropbox.removeChild(plusPlaceholder);
+	}
 }
 
 dropbox.addEventListener("dragover", dragover, false);
@@ -61,19 +115,19 @@ dropbox.addEventListener("click", openInput, false);
 function dragover(e) {
 	e.stopPropagation();
 	e.preventDefault();
-    dropbox.classList.add("over")
+	dropbox.classList.add("over");
 }
 
 function dragleave(e) {
 	e.stopPropagation();
 	e.preventDefault();
-    dropbox.classList.remove("over")
+	dropbox.classList.remove("over");
 }
 
 function drop(e) {
 	e.stopPropagation();
 	e.preventDefault();
-    dropbox.classList.remove("over")
+	dropbox.classList.remove("over");
 
 	var dt = e.dataTransfer;
 	var files = dt.files;
